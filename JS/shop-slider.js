71c32a100fa4b30038838d70a8b2d254596bc8ac
Dasha Kachur <36744584+darjakruglova0@users.noreply.github.com>
2020-01-16 18:18:38 +0300

    Add files via upload

---
+++ JS/shop-slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    const shopSlider = $(".owl-carousel");
+    shopSlider.owlCarousel({
+        items: 3,
+        dots: false,
+        smartSpeed: 500
+    });
+});
